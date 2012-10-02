b93cc76b7d198301fe43ad1e9e2d8a0bc1908e75
Aubrey <aubrey.goodman@gmail.com>
2012-10-02 12:44:10 -0400

    updated to explicitly define getter/setter for block property

--- Whack/UIViewController+Whack.h
+++ Whack/UIViewController+Whack.h
@@ -1,3 +1,15 @@
+//
+//  UIViewController+Whack.h
+//  Whack
+//
+//  Created by Aubrey Goodman on 3/19/11.
+//  Copyright 2012 Migrant Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
 @interface UIViewController (Whack)
 
 @property (copy) dispatch_block_t shakeTriggerBlock;
--- Whack/UIViewController+Whack.m
+++ Whack/UIViewController+Whack.m
@@ -1,10 +1,49 @@
-#import "UIViewController+Whack.h"
+//
+//  UIViewController+Whack.m
+//  Whack
+//
+//  Created by Aubrey Goodman on 3/19/11.
+//  Copyright 2012 Migrant Studios. All rights reserved.
+//
 
-@synthesize shakeTriggerBlock = _shakeTriggerBlock;
+#import "UIViewController+Whack.h"
+#import "objc/runtime.h"
+
+
+static void* kShakeTriggerBlockKey = @"WhackShakeTriggerKey";
+
+@implementation UIViewController (Whack)
+
+- (void)setShakeTriggerBlock:(dispatch_block_t)aShakeTriggerBlock
+{
+	objc_setAssociatedObject(self, kShakeTriggerBlockKey, aShakeTriggerBlock, OBJC_ASSOCIATION_COPY);
+}
+
+- (dispatch_block_t)shakeTriggerBlock
+{
+    return objc_getAssociatedObject(self, kShakeTriggerBlockKey);
+}
 
 - (void)registerShakeTrigger:(dispatch_block_t)aBlock
 {
 	self.shakeTriggerBlock = aBlock;
+}
+
+#pragma mark -
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self becomeFirstResponder];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self resignFirstResponder];
 }
 
 #pragma mark -
@@ -14,3 +53,4 @@
 	dispatch_async(dispatch_get_main_queue(), self.shakeTriggerBlock);
 }
 
+@end
